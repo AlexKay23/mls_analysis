9fd55c730c0671b943bac58f18cf55c710072f4b
AlexKay23 <ak23alex@gmail.com>
2025-03-10 18:18:44 -0400

    added df for team logos for plotting

---
+++ logo_df.R
@@ -0,0 +1,13 @@
+library(rvest)
+
+
+logo_links = "https://www.mlssoccer.com/clubs/"
+page = read_html(logo_links)
+
+teams= page %>% html_nodes(".mls-o-clubs-hub-clubs-list__club-name span") %>% html_text()
+logo= page %>% html_nodes("#main-content .img-responsive") %>% html_attr("src")
+
+team_df <- as_tibble(teams) %>% filter(!value == "") %>% rename(team_name = value)
+logo_df <- as_tibble(logo) %>% rename(logo_url = value)
+
+team_logos_df <- bind_cols(team_df,logo_df)
